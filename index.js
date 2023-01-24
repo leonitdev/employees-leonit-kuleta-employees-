373caa618b2fe01dd15b0cbb2e5da7e391f6a062
leonitdev <leonitkuleta@gmail.com>
2023-01-24 20:56:09 +0100

    added findLongestTeam function

--- index.js
+++ index.js
@@ -59,3 +59,20 @@ const sortDataByDuration = (data) => {
 }
 
 
+const findLongestTeam = (sortedData) => {
+  let longestTeam = {pair1:{duration:0}, pair2:{duration:0}};
+  for(let i = 0; i < sortedData.length - 1; i++){
+    for(let j = i+1; j < sortedData.length - 1; j++){
+      if(sortedData[i].projectId === sortedData[j].projectId && sortedData[j].duration > longestTeam.pair2.duration){
+        longestTeam = {pair1: sortedData[i], pair2:sortedData[j]};
+      }
+    }
+  }
+  return longestTeam;
+}
+
+
+const sortedData = sortDataByDuration(dataSet);
+const longestTeam = findLongestTeam(sortedData);
+
+console.log("longestTeam: ",longestTeam);
