0bea2263608013061ac22eb7b172e3ba58708a5d
leonitdev <leonitkuleta@gmail.com>
2023-01-24 21:34:12 +0100

    got data from json

--- index.js
+++ index.js
@@ -1,51 +1,6 @@
 
-let dataSet = [
-  {
-    empId: 143,
-    projectId:12,
-    dateFrom:"2013-11-01",
-    dateTo:"2014-01-05",
-  },
-  
-    {
-    empId: 218,
-    projectId:10,
-    dateFrom:"2012-05-16",
-    dateTo:null,
-  },
-    {
-    empId: 143,
-    projectId:10,
-    dateFrom:"2009-01-01",
-    dateTo:"2011-04-27",
-  },
-    {
-    empId: 143,
-    projectId:12,
-    dateFrom:"2009-01-05",
-    dateTo:"2011-04-27",
-  },
-  
-      {
-    empId: 149,
-    projectId:14,
-    dateFrom:"2000-01-01",
-    dateTo:"2020-04-27",
-  },
-    {
-    empId: 149,
-    projectId:14,
-    dateFrom:"2007-01-05",
-    dateTo:"2019-04-27",
-  },
-  
-   {
-    empId: 150,
-    projectId:14,
-    dateFrom:"2007-01-05",
-    dateTo:"2020-04-27",
-  },
-];
+
+let dataSet = require('./data/db.json');
 
 const sortDataByDuration = (data) => {
   return data.map(obj => {
